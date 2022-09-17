7e29836a0465ff168d138935a2a929c6a06b0eab
Skes98055 <91359104+Skes98055@users.noreply.github.com>
2022-09-17 22:18:33 +0800

    Add files via upload

---
+++ Class7.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class CTest{
+    int a;int b;int c;
+    void ans(int a1,int b1,int c1){
+        if(a1==0){
+            System.out.println("此數為0");
+        }
+        else if(a1%2==0){
+            System.out.println("此數為偶數");
+        }
+        else if(a1%2!=0){
+            System.out.println("此數為奇數");
+        }
+        if(b1==0){
+            System.out.println("此數為0");
+        }
+        else if(b1%2==0){
+            System.out.println("此數為偶數");
+        }
+        else if(b1%2!=0){
+            System.out.println("此數為奇數");
+        }
+        if(c1==0){
+            System.out.println("此數為0");
+        }
+        else if(c1%2==0){
+            System.out.println("此數為偶數");
+        }
+        else if(c1%2!=0){
+            System.out.println("此數為奇數");
+        }
+    }
+}
+public class Class7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        CTest run=new CTest();
+        run.ans(sc.nextInt(),sc.nextInt(),sc.nextInt());
+        
+    }
+}
