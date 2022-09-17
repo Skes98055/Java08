08ab04b02481f4a4eb8f44944217f6e8097765a6
Skes98055 <91359104+Skes98055@users.noreply.github.com>
2022-09-17 15:21:57 +0800

    Update Class02.java

--- Class02.java
+++ Class02.java
@@ -8,7 +8,7 @@ public class Class02 {
         Cbbb obj2=new Cbbb();
         Cbbb avg =new Cbbb();
         obj1.x=5.2;obj1.y=3.9;
-        obj1.x=6.5;obj2.y=4.6;
+        obj2.x=6.5;obj2.y=4.6;
         avg.x=(obj1.x+obj2.x)/2;
         avg.y=(obj1.y+obj2.y)/2;
         System.out.println(obj1.x+"　"+obj1.y);
